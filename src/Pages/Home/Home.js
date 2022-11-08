d6ccc07e373a60f74e88ad81951184dfb83fa9bf
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-08 14:48:28 +0600

    added home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa";
+import chemistry_animation from '../../Assets/chemistry-animation.json'
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+      <section className="text-gray-800">
+        <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-16 lg:flex-row lg:justify-evenly">
+          <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+            <div className="uppercase mb-7">
+              <h4 className="text-3xl">
+                A{" "}
+                <span className="text-orange-500 font-bold">
+                  Learning Platform
+                </span>
+              </h4>
+              <p className="text-2xl">that helps you to Learn</p>
+              <h1 className="text-6xl text-white bg-emerald-500 font-bold border-2 py-2 px-2">
+                CHEMISTRY
+              </h1>
+              <p className="text-2xl">
+                in a <span className="text-orange-500 font-bold">Fun</span> way
+              </p>
+            </div>
+            <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+              <Link
+                to="/"
+                className="px-5 py-2 text-lg font-semibold border rounded border-orange-600 flex items-center uppercase hover:bg-orange-400 hover:text-white"
+              >
+                Explore All Courses
+                <span className="ml-2">
+                  <FaArrowRight></FaArrowRight>
+                </span>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+            <Lottie animationData={chemistry_animation} loop={true} />;
+          </div>
         </div>
+      </section>
     );
 };
 
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -5,16 +5,16 @@ import logo from '../../Assets/chemistry.png'
 const Header = () => {
      const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-      <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div class="relative flex items-center justify-between">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
           <Link
             to="/"
             aria-label="Company"
             title="Company"
-            class="inline-flex items-center"
+            className="inline-flex items-center"
           >
             <img className="h-14 w-full" src={logo} alt="" />
-            <span class="ml-2 text-2xl font-bold font-mono tracking-wide uppercase">
+            <span className="ml-2 text-2xl font-bold font-mono tracking-wide uppercase">
               <span className="bg-emerald-600 text-white border-2 border-emerald-600 py-1 px-2 rounded-l-md">
                 CHEM
               </span>
@@ -23,13 +23,13 @@ const Header = () => {
               </span>
             </span>
           </Link>
-          <ul class="items-center hidden space-x-8 lg:flex">
+          <ul className="items-center hidden space-x-8 lg:flex">
             <li>
               <Link
                 to="/home"
                 aria-label="Home"
                 title="Home"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
                 Home
               </Link>
@@ -39,7 +39,7 @@ const Header = () => {
                 to="/"
                 aria-label="Our product"
                 title="Our product"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
                 Features
               </Link>
@@ -49,7 +49,7 @@ const Header = () => {
                 to="/"
                 aria-label="Product pricing"
                 title="Product pricing"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
                 Pricing
               </Link>
@@ -59,20 +59,20 @@ const Header = () => {
                 to="/"
                 aria-label="About us"
                 title="About us"
-                class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
                 About us
               </Link>
             </li>
           </ul>
-          <div class="lg:hidden">
+          <div className="lg:hidden">
             <button
               aria-label="Open Menu"
               title="Open Menu"
-              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
               onClick={() => setIsMenuOpen(true)}
             >
-              <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -88,18 +88,18 @@ const Header = () => {
               </svg>
             </button>
             {isMenuOpen && (
-              <div class="absolute top-0 left-0 w-full">
-                <div class="p-5 bg-white border rounded shadow-sm">
-                  <div class="flex items-center justify-between mb-4">
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-white border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
                     <div>
                       <Link
                         to="/"
                         aria-label="Company"
                         title="Company"
-                        class="inline-flex items-center"
+                        className="inline-flex items-center"
                       >
                         <img className="h-12 w-full" src={logo} alt="" />
-                        <span class="ml-2 text-xl font-bold tracking-wide uppercase">
+                        <span className="ml-2 text-xl font-bold tracking-wide uppercase">
                           <span className="bg-emerald-600 text-white border-2 border-emerald-600 py-1 px-2 rounded-l-md">
                             CHEM
                           </span>
@@ -113,10 +113,10 @@ const Header = () => {
                       <button
                         aria-label="Close Menu"
                         title="Close Menu"
-                        class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                           <path
                             fill="currentColor"
                             d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -126,13 +126,13 @@ const Header = () => {
                     </div>
                   </div>
                   <nav>
-                    <ul class="space-y-4">
+                    <ul className="space-y-4">
                       <li>
                         <Link
                           to="/home"
                           aria-label="Home"
                           title="Home"
-                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Home
                         </Link>
@@ -142,7 +142,7 @@ const Header = () => {
                           to="/"
                           aria-label="Our product"
                           title="Our product"
-                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Features
                         </Link>
@@ -152,7 +152,7 @@ const Header = () => {
                           to="/"
                           aria-label="Product pricing"
                           title="Product pricing"
-                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Pricing
                         </Link>
@@ -162,7 +162,7 @@ const Header = () => {
                           to="/"
                           aria-label="About us"
                           title="About us"
-                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           About us
                         </Link>
