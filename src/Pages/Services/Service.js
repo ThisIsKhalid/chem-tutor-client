c25e51b30523163616dd5c024e84f1e1032b6e7b
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-08 22:28:58 +0600

    fixed nav problem

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -8,21 +8,23 @@ const Service = ({ service }) => {
   const { _id, name, img, price, description } = service;
 
   return (
-    <div className="card bg-base-100 text-gray-900 shadow-xl rounded-md">
+    <div className="bg-base-100 text-gray-900 shadow-xl rounded-md">
       <PhotoProvider>
         <PhotoView src={img}>
-          <img className="rounded-t-md" src={img}  alt="Shoes" />
+          <img className="rounded-t-md w-full" src={img} alt="Shoes" />
         </PhotoView>
       </PhotoProvider>
       {/* <img className="rounded-t-md" src={img} alt="Shoes" /> */}
       <div className="card-body">
-        <h2 className="card-title text-2xl font-bold">{name}</h2>
+        <h2 className="text-2xl font-bold">{name}</h2>
         <p>{description.slice(0, 100)}</p>
-        <div className="card-actions justify-between items-center">
+        <div className="flex justify-between items-center">
           <p className="text-2xl text-orange-500 font-bold">
             ${price}/<small>month</small>{" "}
           </p>
-          <Link to={`/services/${_id}`} className="btn btn-outline btn-warning">View Details</Link>
+          <Link to={`/services/${_id}`} className="btn btn-outline btn-warning">
+            View Details
+          </Link>
         </div>
       </div>
     </div>
