143c1246cb0d1b83434fe5f75945575b5a37a2f0
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-10 00:54:18 +0600

    added conditional rendering

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -31,13 +31,23 @@ const MyReviews = () => {
 
   return (
     <div>
-      {reviews.map((review) => (
-        <MyReview
-          key={review._id}
-          review={review}
-          handleReveiwDelete={handleReveiwDelete}
-        ></MyReview>
-      ))}
+      {reviews.length ? (
+        <div>
+          {reviews.map((review) => (
+            <MyReview
+              key={review._id}
+              review={review}
+              handleReveiwDelete={handleReveiwDelete}
+            ></MyReview>
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-96">
+          <h2 className="text-red-500 text-3xl font-bold px-5">
+            No reviews were added
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
