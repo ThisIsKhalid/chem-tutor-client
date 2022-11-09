7c533b89cf506857096f241a4410b189e1aaf9a1
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-10 01:25:35 +0600

    added new service

--- src/Pages/AddServices/AddServices.js
+++ src/Pages/AddServices/AddServices.js
@@ -1,6 +1,38 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const AddServices = () => {
+  const handleAddService = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const img = form.img.value;
+    const price = form.price.value;
+    const description = form.description.value;
+
+    const newService = {
+      name,
+      img,
+      price,
+      description,
+    };
+
+    fetch("http://localhost:5000/add_service", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newService),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Successfully added a New Service");
+          form.reset();
+        }
+      });
+  };
+
   return (
     <div>
       <h1 className="text-center text-blue-500 font-bold text-3xl uppercase mb-10">
@@ -8,6 +40,7 @@ const AddServices = () => {
       </h1>
       <div className="lg:w-1/2 md:w-3/4 w-11/12 px-8 py-16 mx-auto rounded-lg  md:px-12 lg:px-16 xl:px-32 text-gray-800 border border-gray-500 bg-gray-100">
         <form
+          onSubmit={handleAddService}
           className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
           <div>
@@ -15,6 +48,7 @@ const AddServices = () => {
               Service Name
             </label>
             <input
+              required
               name="name"
               id="name"
               type="text"
@@ -27,6 +61,7 @@ const AddServices = () => {
               Image URL
             </label>
             <input
+              required
               name="img"
               id="image"
               type="text"
@@ -38,6 +73,7 @@ const AddServices = () => {
               Price
             </label>
             <input
+              required
               name="price"
               id="price"
               type="text"
@@ -49,6 +85,7 @@ const AddServices = () => {
               Description
             </label>
             <textarea
+              required
               name="description"
               id="description"
               rows="3"
