d76b2d5d9947a9b5240fd29d5dd031f01ba09239
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-10 11:15:46 +0600

    added environmental verable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATcSaHy2FHmd2pGE7bThzJ94h983HKl14",
-  authDomain: "chem-tutor.firebaseapp.com",
-  projectId: "chem-tutor",
-  storageBucket: "chem-tutor.appspot.com",
-  messagingSenderId: "803075407029",
-  appId: "1:803075407029:web:9c7d56fa9f6fc54af1d2fc",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
