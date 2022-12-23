673de165dfc8e2a3861682a9bcbb77416ae72a5b
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-23 18:37:08 +0600

    update color in faq

--- src/Pages/Home/ImportantFacts.jsx
+++ src/Pages/Home/ImportantFacts.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import CountUp from "react-countup";
-import { FiUser, FiFile, FiUsers, FiMapPin } from "react-icons/fi";
+import { FiFile, FiMapPin, FiUser, FiUsers } from "react-icons/fi";
 
 const ImportantFacts = () => {
-    return (
-      <section className="container mx-auto py-10 lg:px-0 px-5 bg-slate-100">
+  return (
+    <section className="py-10 bg-slate-100">
+      <div className="container mx-auto lg:px-0 px-5">
         <div className="flex flex-col items-center justify-center mb-10">
           <h1 className="uppercase text-3xl font-semibold text-center">
             Some Important
@@ -13,30 +14,31 @@ const ImportantFacts = () => {
           <hr className="w-96 border-gray-400 mt-5" />
           <hr className="w-80 mt-3 border-gray-400" />
         </div>
-        <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-3">
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2">
-            <FiUser className="h-12 w-12 text-emerald-500" />
+        <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-3 text-white">
+          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
+            <FiUser className="h-12 w-12" />
             <CountUp className="text-4xl font-medium" end={55} duration={4} />
-            <h1 className="text-2xl text-emerald-500 uppercase">Teachers</h1>
+            <h1 className="text-2xl uppercase">Teachers</h1>
           </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2">
-            <FiFile className="h-12 w-12 text-emerald-500" />
+          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
+            <FiFile className="h-12 w-12" />
             <CountUp className="text-4xl font-medium" end={199} duration={4} />
-            <h1 className="text-2xl text-emerald-500 uppercase">Courses</h1>
+            <h1 className="text-2xl uppercase">Courses</h1>
           </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2">
-            <FiUsers className="h-12 w-12 text-emerald-500" />
+          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
+            <FiUsers className="h-12 w-12" />
             <CountUp className="text-4xl font-medium" end={2320} duration={4} />
-            <h1 className="text-2xl text-emerald-500 uppercase">Members</h1>
+            <h1 className="text-2xl uppercase">Members</h1>
           </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2">
-            <FiMapPin className="h-12 w-12 text-emerald-500" />
+          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
+            <FiMapPin className="h-12 w-12" />
             <CountUp className="text-4xl font-medium" end={27} duration={4} />
-            <h1 className="text-2xl text-emerald-500 uppercase">Countries</h1>
+            <h1 className="text-2xl uppercase">Countries</h1>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default ImportantFacts;
