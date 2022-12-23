92b3e8457b22ae3c56489e317546a481cebccdaf
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-23 19:46:33 +0600

    add react scroll trigger

--- src/Pages/Home/ImportantFacts.jsx
+++ src/Pages/Home/ImportantFacts.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import CountUp from "react-countup";
+import ScrollTrigger from 'react-scroll-trigger'
 import { FiFile, FiMapPin, FiUser, FiUsers } from "react-icons/fi";
+import { useState } from "react";
 
 const ImportantFacts = () => {
+  const [counterOn, setCounterOn] = useState(false);
+
   return (
     <section className="py-10 bg-slate-100">
       <div className="container mx-auto lg:px-0 px-5">
@@ -14,28 +18,41 @@ const ImportantFacts = () => {
           <hr className="w-96 border-gray-400 mt-5" />
           <hr className="w-80 mt-3 border-gray-400" />
         </div>
-        <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-3 text-white">
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
-            <FiUser className="h-12 w-12" />
-            <CountUp className="text-4xl font-medium" end={55} duration={4} />
-            <h1 className="text-2xl uppercase">Teachers</h1>
+        <ScrollTrigger
+          onEnter={() => setCounterOn(true)}
+          onExit={() => setCounterOn(false)}
+        >
+          <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-5 gap-3 text-white">
+            <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
+              <FiUser className="h-12 w-12" />
+              <h1 className="text-4xl font-medium">
+                {counterOn && <CountUp end={55} duration={5} delay={0} />}+
+              </h1>
+              <h1 className="text-2xl uppercase">Teachers</h1>
+            </div>
+            <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
+              <FiFile className="h-12 w-12" />
+              <h1 className="text-4xl font-medium">
+                {counterOn && <CountUp end={199} duration={5} delay={0} />}+
+              </h1>
+              <h1 className="text-2xl uppercase">Courses</h1>
+            </div>
+            <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
+              <FiUsers className="h-12 w-12" />
+              <h1 className="text-4xl font-medium">
+                {counterOn && <CountUp end={2320} duration={5} delay={0} />}+
+              </h1>
+              <h1 className="text-2xl uppercase">Members</h1>
+            </div>
+            <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
+              <FiMapPin className="h-12 w-12" />
+              <h1 className="text-4xl font-medium">
+                {counterOn && <CountUp end={27} duration={5} delay={0} />}+
+              </h1>
+              <h1 className="text-2xl uppercase">Countries</h1>
+            </div>
           </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
-            <FiFile className="h-12 w-12" />
-            <CountUp className="text-4xl font-medium" end={199} duration={4} />
-            <h1 className="text-2xl uppercase">Courses</h1>
-          </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-green-800">
-            <FiUsers className="h-12 w-12" />
-            <CountUp className="text-4xl font-medium" end={2320} duration={4} />
-            <h1 className="text-2xl uppercase">Members</h1>
-          </div>
-          <div className="border border-gray-300 flex flex-col items-center justify-center p-5 rounded-md gap-2 bg-rose-800">
-            <FiMapPin className="h-12 w-12" />
-            <CountUp className="text-4xl font-medium" end={27} duration={4} />
-            <h1 className="text-2xl uppercase">Countries</h1>
-          </div>
-        </div>
+        </ScrollTrigger>
       </div>
     </section>
   );
---
+++ src/Pages/Home/Testimonial.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Swiper from "swiper";
+import "swiper/css";
+
+const Testimonial = () => {
+  return <div></div>;
+};
+
+export default Testimonial;
