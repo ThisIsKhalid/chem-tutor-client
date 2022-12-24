927a7ee7d08da6d04aa093adae23ad06d0d27e13
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-24 10:56:43 +0600

    update newsletter

--- src/Pages/Home/Subscribe.js
+++ src/Pages/Home/Subscribe.js
@@ -14,51 +14,50 @@ const Subscribe = () => {
     }
   };
   return (
-    <div className="bg-cyan-900">
-      <div className="container px-6 lg:py-2 py-10 mx-auto">
-        <div className="lg:flex flex-col md:flex-row items-center flex">
-          <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-            <img
-              className="w-full h-full max-w-md"
-              src={subscribelogo}
-              alt="email illustration vector art"
+    <section className="bg-gray-100 py-10">
+      <div className="container px-6  mx-auto">
+        <div className="flex flex-col items-center justify-center mb-5">
+          <h1 className="uppercase text-3xl font-semibold text-center">
+            Subscribe To The
+            <span className="text-emerald-500"> Newsletter</span>
+          </h1>
+          <p className="mt-2 text-gray-800">
+            be the first to knows when our new{" "}
+            <span className="font-semibold text-emerald-500">Courses</span>{" "}
+            arrive.
+          </p>
+          <hr className="w-96 border-gray-400 mt-5" />
+          <hr className="w-80 mt-3 border-gray-400" />
+        </div>
+        <div className="flex items-center justify-center">
+          <form
+            onSubmit={handleSubscribe}
+            className="md:w-1/2 w-full flex flex-col gap-3"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="input input-bordered input-success w-full"
             />
-          </div>
-          <div className="w-full lg:w-1/2">
-            <div className="lg:max-w-lg">
-              <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
-                Subscribe To The{" "}
-                <span className="text-blue-500">Newsletter</span>
-              </h1>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered input-success w-full"
+            />
+            <textarea
+              className="textarea textarea-success"
+              placeholder="Bio"
+            ></textarea>
 
-              <p className="mt-4 text-gray-600 dark:text-gray-400">
-                be the first to knows when our new{" "}
-                <span className="font-semibold text-blue-500">Courses</span>{" "}
-                arrive.
-              </p>
-
-              <form
-                onSubmit={handleSubscribe}
-                className="flex flex-col mt-8 space-y-3 lg:space-y-0 lg:flex-row"
-              >
-                <input
-                  required
-                  name="email"
-                  id="email"
-                  type="email"
-                  className="px-4 py-2 text-gray-800 bg-gray-100 border rounded-md  focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-                  placeholder="Email Address"
-                />
-
-                <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto lg:mx-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                  Subscribe
-                </button>
-              </form>
-            </div>
-          </div>
+            <button className="py-2 px-5 bg-emerald-500 rounded-md text-xl font-medium text-white">
+              Subscribe
+            </button>
+          </form>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
